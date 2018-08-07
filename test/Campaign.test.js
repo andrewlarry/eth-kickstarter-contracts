968f29813beb68e0cbfab98d9d9b32026fc6a6ef
Andrew <afuselier23@gmail.com>
2018-08-07 11:23:45 -0700

    setting up testing structure

--- test/Campaign.test.js
+++ test/Campaign.test.js
@@ -7,44 +7,86 @@ const ganche = require('ganache-cli');
 const Web3 = require('web3');
 
 // A provider is a link to a running Etherem node
-const provider = ganche.provider();
+const web3 = new Web3(ganche.provider());
 
-const web3 = new Web3(provider);
+const compiledFactory = require('../build/CampaignFactory.json');
+const comiledCampaign = require('../build/Campaign.json');
 
-const { interface, bytecode } = require('../compile');
-
+// beforeEach variables
 let accounts;
-let inbox; 
-const INITIAL_STRING = 'Hi there!';
+let factory;
+let campaign;
+let campaignAddress; 
 
 beforeEach(async () => {
   // Get a list of all accounts
   accounts = await web3.eth.getAccounts();
 
-  // Use one of those accounts to deploy contract
-
-  // JSON.parse(interface) = JS object that represents the ABI
-  inbox = await new web3.eth.Contract(JSON.parse(interface))
-    .deploy({ data: bytecode, arguments: [INITIAL_STRING] })
+  // Use one of those accounts to deploy factory
+  factory = await new web3.eth.Contract(JSON.parse(compiledFactory.interface))
+    .deploy({ data: compiledFactory.bytecode })
     .send({ from: accounts[0], gas: '1000000' });
 
-  inbox.setProvider(provider);
+  // Call the createCampaign method to deploy a campaign contract instance
+  // A 'send' does not return a value, just a tx reciept
+  await factory.methods.createCampaign('100').send({ 
+    from: accounts[0], 
+    gas: '1000000'
+  });
+  
+  // Get the list of campaigns (should only be one)
+  const campaigns = await factory.methods.getDeloyedCampaigns().call();
+  campaignAddress = campaigns[0];
+
+  // Create the JS API for the deployed campaign contract, no need to deploy or send a tx
+  campaign = await new web3.eth.Contract(
+    JSON.parse(comiledCampaign.interface),
+    campaignAddress
+  );
 });
 
-describe('Inbox', () => {
-  it('Deploys a contract', () => {
-    // Fails if argument is null or undefined
-    assert.ok(inbox.options.address);
+describe('Campaigns', () => {
+  it('Deploys a factory and campaign contract', () => {
+    assert.ok(factory.options.address);
+    assert.ok(campaign.options.address);
   });
 
-  it('Initializes message with constructor argument', async () => {
-    const message = await inbox.methods.message().call();
-    assert.equal(message, INITIAL_STRING);
+  it('Marks caller as the campaign manager', async () => {
+    const manager = await campaign.methods.manager().call();
+    assert.equal(accounts[0], manager);
   });
 
-  it('Sets a new message with the setMessage() method', async () => {
-    await inbox.methods.setMessage('testing').send({ from: accounts[0] });
-    const message = await inbox.methods.message().call();
-    assert.equal(message, 'testing');
+  it('Adds contributor to the campaign as an approver', async () => {
+    await campaign.methods.contribute().send({
+      from: accounts[1],
+      value: '101'
+    });
+
+    const isContributor = await campaign.methods.approvers(accounts[1]).call();
+    assert(isContributor);
+  });
+
+  it('Requires a minimum contribution', async () => {
+    try {
+      await campaign.methods.contribute().send({
+        from: accounts[2],
+        value: '50'
+      });
+      assert(false);
+    } catch(err) {
+      assert(err);
+    }
+  });
+
+  it('Allows a manager to make a payment request', async ()  => {
+    await campaign.methods
+      .createRequest('test', '100', accounts[1])
+      .send({
+        from: accounts[0],
+        gas: '1000000'
+      });
+    
+    const request = await campaign.methods.requests(0).call();
+    assert.equal('test', request.description);
   });
 });
