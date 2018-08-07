3dc68bf034246d0898c989559514820104bf23f6
Andrew <afuselier23@gmail.com>
2018-08-06 20:48:02 -0700

    refactor compile script to write output to build directory

--- compile.js
+++ compile.js
@@ -1,12 +1,26 @@
 const path = require('path');
-const fs = require('fs');
+const fs = require('fs-extra');
 const { compile } = require('solc');
 
-// Path to a smart contract
+// Get path to build directory and remove
+const buildPath = path.resolve(__dirname, 'build');
+fs.removeSync(buildPath)
+
+// Path to smart contract
 const contractPath = path.resolve(__dirname, 'contracts', 'Campaign.sol');
 
-// Read the contract file and encode utf8
+// Read the contract file and compile with solc
 const source = fs.readFileSync(contractPath, 'utf8');
+const compiled = compile(source, 1).contracts;
 
-module.exports = compile(source, 1).contracts[':Campaign'];
+// Rebuild the build directory
+fs.ensureDirSync(buildPath);
+
+// Write compiled contracts to build directory
+for (let contract in compiled) {
+  fs.outputJsonSync(
+    path.resolve(buildPath, contract.substring(1) + '.json'),
+    compiled[contract]
+  );
+}
 
